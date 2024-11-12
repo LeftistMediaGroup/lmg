7f8cbedf6afc65c9c18849ab37cde71406407c24
Leftist Media Group <LeftistMediaGroup@Gmail.com>
2024-11-11 20:59:55 -0500

    U Package.json

--- src/modules/account/dashboard/Journal/MoodChart.jsx
+++ src/modules/account/dashboard/Journal/MoodChart.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 import { Card, CardContent } from "@mui/material";
 
-
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-
 export default class MoodChart extends Component {
   constructor(props) {
     super(props);
